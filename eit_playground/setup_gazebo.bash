10efe405156081c75d031391fb1be427f720081a
marcus-grove <mgrov16@student.sdu.dk>
2020-10-01 11:57:55 +0200

    eit_playground/

--- eit_playground/setup_gazebo.bash
+++ eit_playground/setup_gazebo.bash
@@ -2,7 +2,7 @@
 
 export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:/home/$USER/Firmware
 export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:/home/$USER/Firmware/Tools/sitl_gazebo
-export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$PWD/src/eit_playground/models
+export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$PWD/src/EiT_group5/eit_playground/models
 
 
 echo ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH
